478f44a35ebc2821d13e8a9ff62d59747f50a317
AHartmaier <alexander.hartmaier@rub.de>
2025-08-29 11:16:07 +0200

    Updated unit tests

--- tests/test_texture.py
+++ tests/test_texture.py
@@ -24,7 +24,7 @@ def test_mex():
 
 @pytest.mark.skipif(not  __backend__=='mtex', reason="Kanapy is not configured for texture analysis yet!")
 def test_readEBSD():
-    from kanapy_mtex.textures_mtex import EBSDmap
+    from kanapy_mtex.texture import EBSDmap
     # name of ang file to be imported
     fname = os.path.normpath(os.path.join(os.getcwd(), 'tests', 'ebsd_316L_500x500.ang'))
     # read EBSD map and evaluate statistics of microstructural features
@@ -38,7 +38,7 @@ def test_readEBSD():
 
 @pytest.mark.skipif(not  __backend__=='mtex', reason="Kanapy is not configured for texture analysis yet!")
 def test_createORI():
-    from kanapy_mtex.textures_mtex import createOriset
+    from kanapy_mtex.texture import createOriset
     Ngr = 10
     ang = [0., 45., 0.]    # Euler angles for Goss texture
     omega = 7.5         # kernel half-width
